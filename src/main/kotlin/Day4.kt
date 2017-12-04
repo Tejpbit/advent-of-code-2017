5e38b117d07247fb7e8d806e27f041e7dbde0f7c
André Samuelsson <andresamuelsson94@gmail.com>
2017-12-04 14:08:51 +0100

    Filter and count instead of map and count { it }, day 4

--- src/main/kotlin/Day4.kt
+++ src/main/kotlin/Day4.kt
@@ -1,10 +1,10 @@
 class Day4: Day<List<List<String>>> {
     override fun parse(input: String): List<List<String>> {
-        return input.lines().map { it -> it.split("\\s".toRegex()) }
+        return input.lines().map { it.split("\\s".toRegex()) }
     }
 
     override fun part1(input: List<List<String>>): Any {
-        return input.map { it -> isValid(it) }.count { it }
+        return input.filter { isValid(it) }.count()
     }
 
     fun isValid(passphrase: List<*>): Boolean {
@@ -15,7 +15,7 @@ class Day4: Day<List<List<String>>> {
         val sortedLists = input.map {
             sublist -> sublist.map { word -> word.toList().sorted() }
         }
-        return sortedLists.map { isValid(it) }.count {it}
+        return sortedLists.filter { isValid(it) }.count()
 
     }
 
